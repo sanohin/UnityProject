16d3a0cda960bc472919c6236bd93c7ab5ff974f
Aleksandr Anokhin <anohin@newtonideas.com>
2017-05-23 23:33:00 +0200

    Task 2

--- Assets/Scritps/Rabbit.cs
+++ Assets/Scritps/Rabbit.cs
@@ -1,51 +1,94 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//
-//public class Rabbit : MonoBehaviour {
-//	public float speed = 50f;
-//	public float jumpPower = 150f;
-//	public bool grounded;
-//
-//	private Rigidbody2D rb2d;
-//	// Use this for initialization
-//	void Start () {
-//		rb2d = gameObject.GetComponent<Rigidbody2D>();
-//	}
-//	
-//	// Update is called once per frame
-//	void Update () {
-//		float h = Input.GetAxis ("Horizontal");
-//		rb2d.AddForce ((Vector2.right * speed) * h);
-//	}
-//}
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rabbit : MonoBehaviour {
-	public float speed = 1;
-	Rigidbody2D myBody = null;
-	// Use this for initialization
-	void Start () {
-		myBody = this.GetComponent<Rigidbody2D> ();
-	}
+public class Rabbit : MonoBehaviour
+{
 
-	// Update is called once per frame
-	void Update () {
+    public float speed = 1;
 
-	}
-	void FixedUpdate () { //[-1, 1]
-		float value = Input.GetAxis ("Horizontal");
-		if (Mathf.Abs (value) > 0) {
-			Vector2 vel = myBody.velocity;
-			vel.x = value * speed;
-			myBody.velocity = vel;
-		} 
-		SpriteRenderer sr = GetComponent<SpriteRenderer>(); if(value < 0) {
-			sr.flipX = true;
-		} else if(value > 0) {
-			sr.flipX = false;
-		}
-	}
+    bool isGrounded = false;
+    bool JumpActive = false;
+    float JumpTime = 0f;
+    public float MaxJumpTime = 2f;
+    public float JumpSpeed = 2f;
+
+    Rigidbody2D body = null;
+    SpriteRenderer sr = null;
+    Animator animator = null;
+
+    void Start()
+    {
+        body = this.GetComponent<Rigidbody2D>();
+        sr = this.GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        LevelController.current.setStartPosition (transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float value = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(value) > 0.0)
+        {
+            Vector2 vel = body.velocity;
+            vel.x = value * speed;
+            body.velocity = vel;
+        }
+
+        animator.SetFloat("speed", Mathf.Abs(value) * speed);
+
+        if (value > 0)
+        {
+            sr.flipX = false;
+        }
+        else if (value < 0)
+        {
+            sr.flipX = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 from = transform.position + Vector3.up * 0.3f; Vector3 to = transform.position + Vector3.down * 0.1f; int layer_id = 1 << LayerMask.NameToLayer("Ground");
+
+        RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
+
+        if (hit)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        Debug.DrawLine(from, to, Color.red);
+
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            this.JumpActive = true;
+        }
+
+        if (this.JumpActive)
+        {
+            //Якщо кнопку ще тримають 
+            if (Input.GetButton("Jump"))
+            {
+                this.JumpTime += Time.deltaTime;
+                if (this.JumpTime < this.MaxJumpTime)
+                {
+                    Vector2 vel = body.velocity;
+                    vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime); body.velocity = vel;
+                }
+            }
+            else
+            {
+                this.JumpActive = false;
+                this.JumpTime = 0;
+            }
+        }
+        Animator animator = GetComponent<Animator>();
+        animator.SetBool("grounded", this.isGrounded);
+    }
 }
